65e31258685f95295752cf8492f7d533df36de93
adrianfabianferrer05-dot <adrianfabianferrer05@gmail.com>
2026-09-24 06:21:03 +0200

    Forzar actualización de la PWA con la rutina visible

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='segunda-mente-v36';
+const CACHE='segunda-mente-v37';
 const STATE_CACHE='segunda-mente-state-v1';
 const PUSH_MARKER='./__push_registered__';
 const BANK_URL='https://dabzmzwnvzoeywyflkoo.supabase.co/functions/v1/bank';
